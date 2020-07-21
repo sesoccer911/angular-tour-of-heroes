a28686e60ef8ecfab1a9784af00a08486f09d1d0
Steven Green <sesoccer911@aol.com>
2020-07-21 15:27:16 -0700

    Fixing incorrect dependencies

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,33 +1,18 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
 import { Hero } from '../hero';
-import { HeroService } from '../hero.service';
-import { MessageService } from '../message.service';
 
 @Component({
-  selector: 'app-heroes',
-  templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  selector: 'app-hero-detail',
+  templateUrl: './hero-detail.component.html',
+  styleUrls: ['./hero-detail.component.css']
 })
-export class HeroesComponent implements OnInit {
+export class HeroDetailComponent implements OnInit {
 
-  selectedHero: Hero;
+  @Input() hero: Hero;
 
-  heroes: Hero[];
-
-  constructor(private heroService: HeroService, private messageService: MessageService) { }
+  constructor() { }
 
   ngOnInit() {
-    this.getHeroes();
   }
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
-  }
-
-  getHeroes(): void {
-    this.heroService.getHeroes()
-        .subscribe(heroes => this.heroes = heroes);
-  }
 }
