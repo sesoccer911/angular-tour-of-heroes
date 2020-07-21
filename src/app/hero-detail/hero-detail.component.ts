24e97b2ab46523e5dbbadfa2ee0e9ac6d30efd82
Steven Green <sesoccer911@aol.com>
2020-07-21 15:16:57 -0700

    You refactored data access to the HeroService class. You registered the HeroService as the provider of its service at the root level so that it can be injected anywhere in the app. You used Angular Dependency Injection to inject it into a component. You gave the HeroService get data method an asynchronous signature. You discovered Observable and the RxJS Observable library. You used RxJS of() to return an observable of mock heroes (Observable<Hero[]>). The component's ngOnInit lifecycle hook calls the HeroService method, not the constructor. You created a MessageService for loosely-coupled communication between classes. The HeroService injected into a component is created with another injected service, MessageService.

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,17 +1,33 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
-  selector: 'app-hero-detail',
-  templateUrl: './hero-detail.component.html',
-  styleUrls: ['./hero-detail.component.css']
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
 })
-export class HeroDetailComponent implements OnInit {
-  @Input() hero: Hero;
+export class HeroesComponent implements OnInit {
 
-  constructor() { }
+  selectedHero: Hero;
+
+  heroes: Hero[];
+
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
 
   ngOnInit() {
+    this.getHeroes();
   }
 
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
+  }
 }
